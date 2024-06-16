2d80ae13a22ce132d1989ff750a042f281390352
David <davidmanasmoreno@gmail.com>
2024-06-16 14:18:21 +0200

    sobre mi casi terminado

--- app/vistas/sobreMi.php
+++ app/vistas/sobreMi.php
@@ -33,85 +33,109 @@
             </div>
             <div class="about-content">
                 <h2>Sobre mí</h2>
-                <p>Hola, soy [Tu Nombre]. Soy [tu profesión/ocupación] con pasión por [tu pasión o hobby].</p>
-                <p>Tengo [X] años de experiencia en [tu campo/industria] y he trabajado en [menciona algunos lugares o proyectos importantes].</p>
-                <p>En mi tiempo libre, disfruto [menciona tus hobbies o intereses]. Me apasiona [algo que te apasione] y siempre estoy buscando nuevas maneras de [algo que te guste hacer].</p>
-                <p>Estoy comprometido/a con [menciona alguna causa, proyecto o meta personal].</p>
-                <p>Gracias por visitar mi página. ¡Espero que disfrutes aprendiendo más sobre mí!</p>
+                <p>Hola, soy David Mañas Moreno, un apasionado desarrollador de aplicaciones web con una sólida trayectoria en el ámbito tecnológico.</p>
+                <p>Cuento con 1 año de experiencia en el desarrollo de software y he participado en diversos proyectos importantes, colaborando con empresas reconocidas y equipos multidisciplinarios.</p>
+                <p>En mi tiempo libre, disfruto de la fotografía y el deporte, lo que me permite mantener un equilibrio saludable entre mi vida personal y profesional. Me apasiona programar y siempre estoy buscando nuevas maneras de innovar y mejorar mis habilidades.</p>
+                <p>Estoy comprometido con [menciona alguna causa, proyecto o meta personal], lo que refleja mi dedicación tanto en el ámbito profesional como en el personal.</p>
+                <p>Gracias por tomarte el tiempo de conocerme. ¡Espero que disfrutes aprendiendo más sobre mí y mi trabajo!</p>
             </div>
+
         </div>
 
         <div class="container">
-        <div class="left-column">
-            <div class="profile-pic">
-                <img src="web/miFoto.jpg" alt="David Mañas Moreno">
+            <div class="left-column">
+                <div class="profile-pic">
+                    <img src="web/miFoto.jpg" alt="David Mañas Moreno">
+                </div>
+                <h1>David Mañas Moreno</h1>
+                <div class="contact-info">
+                    <p><i class="fas fa-envelope"></i> davidmanasmoreno@gmail.com</p>
+                    <p><i class="fas fa-phone"></i> 673556926</p>
+                    <p><i class="fas fa-map-marker-alt"></i> Calle Monje Nº 75 Socuéllamos (Ciudad Real)</p>
+                </div>
+                <div class="more-info">
+                    <h2>Más información</h2>
+                    <p>Permiso de conducir: Tipo B</p>
+                </div>
+                <div class="languages">
+                    <h2>Idiomas</h2>
+                    <p>Castellano</p>
+                    <p>Inglés A2</p>
+                </div>
+                <div class="hobbies">
+                    <h2>Aficiones</h2>
+                    <p>Viajar</p>
+                    <p>Música</p>
+                    <p>Motos</p>
+                </div>
             </div>
-            <h1>David Mañas Moreno</h1>
-            <div class="contact-info">
-                <p><i class="fas fa-envelope"></i> davidmanasmoreno@gmail.com</p>
-                <p><i class="fas fa-phone"></i> 673556926</p>
-                <p><i class="fas fa-map-marker-alt"></i> Calle Monje Nº 75 Socuéllamos (Ciudad Real)</p>
-            </div>
-            <div class="more-info">
-                <h2>Más información</h2>
-                <p>Permiso de conducir: Tipo B</p>
-            </div>
-            <div class="languages">
-                <h2>Idiomas</h2>
-                <p>Castellano</p>
-                <p>Inglés A2</p>
-            </div>
-            <div class="hobbies">
-                <h2>Aficiones</h2>
-                <p>Viajar</p>
-                <p>Música</p>
-                <p>Motos</p>
+            <div class="right-column">
+                <div class="section">
+                    <h2>Habilidades</h2>
+                    <ul>
+                        <li>Aplicaciones Ofimáticas</li>
+                        <li>Conocimientos en configuración de Sistemas operativos</li>
+                        <li>Montaje y Mantenimiento de equipos</li>
+                        <li>Instalación de sistemas operativos</li>
+                        <li>Conocimientos en HTML, CSS, PHP, JavaScript, Java, SQL, Ajax y Spring</li>
+                        <li>Conocimientos en montaje de redes</li>
+                    </ul>
+                </div>
+                <div class="section">
+                    <h2>Perfil personal</h2>
+                    <p>Me considero una persona responsable, creativa, con facilidad de adaptación, capacidad de trabajar en equipo y con ganas de seguir aprendiendo</p>
+                </div>
+                <div class="section">
+                    <h2>Formación</h2>
+                    <p><strong>(2015-2020) ESO</strong><br>
+                        I.E.S Fernando De Mena, Socuéllamos</p>
+                    <p><strong>(2021-2022) GRADO MEDIO SISTEMAS MICROINFORMATICOS Y REDES</strong><br>
+                        I.E.S. Juan Bosco, Alcázar de San Juan</p>
+                    <p><strong>(2022-Actualmente) GRADO SUPERIOR DESARROLLO DE APLICACIONES WEB</strong><br>
+                        I.E.S. Juan Bosco, Alcázar de San Juan</p>
+                </div>
+                <div class="section">
+                    <h2>Experiencia Laboral</h2>
+                    <p><strong>Informático, Ayuntamiento de Socuéllamos</strong><br>
+                        04/07/2022 - 30/06/2023</p>
+                    <ul>
+                        <li>Reparación y Mantenimiento de equipos informáticos</li>
+                        <li>Mantenimiento de redes</li>
+                        <li>Instalación de Sistemas Operativos</li>
+                        <li>Instalación de Programas</li>
+                        <li>Mantenimiento del Servidor y Dominio</li>
+                        <li>Telefonía</li>
+                        <li>Mantenimiento de cuentas de correo electrónico</li>
+                    </ul>
+                </div>
+                <div class="section">
+                    <h2>Cursos</h2>
+                    <p><strong>Plataforma de Formación del Centro de Desarrollo de Competencias Digitales en Castilla-La Mancha</strong><br></p>
+                    <ul>
+                        <li>Administración avanzada de WordPress</li>
+                        <li>Seguridad y auditoría TIC </li>
+                        <li>Gestión de la Identidad Digital en Internet</li>
+                        <li>Dinamización de Puntos de Inclusión Digital y otras Aulas TIC (No tutorizado)</li>
+                        <li>Protección y seguridad informática en la empresa</li>
+                    </ul>
+                    <p><strong>FEMP - Federación Española de Municipios y Provincias</strong><br></p>
+                    <ul>
+                        <li>La e-contratación. Contratación pública electrónica</li>
+                        <li> Mantenimiento web para principales CMS</li>
+                        <li>Interconexión de registros entre administraciones públicas REC, SIR y ORVE</li>
+                        <li>Jornada “Inmersión en AWS” </li>
+                        <li>Comunicación digital y networking en internet </li>
+                    </ul>
+                    <p><strong>Centro Criptológico Nacional. CCN</strong><br></p>
+                    <ul>
+                        <li>Jornadas descentralizadas del ENS</li>
+                    </ul>
+                </div>
             </div>
         </div>
-        <div class="right-column">
-            <div class="section">
-                <h2>Habilidades</h2>
-                <ul>
-                    <li>Aplicaciones Ofimáticas</li>
-                    <li>Conocimientos en configuración de Sistemas operativos</li>
-                    <li>Montaje y Mantenimiento de equipos</li>
-                    <li>Instalación de sistemas operativos</li>
-                    <li>Conocimientos en HTML, CSS, PHP, JavaScript, Java, SQL, Ajax y Spring</li>
-                    <li>Conocimientos en montaje de redes</li>
-                </ul>
-            </div>
-            <div class="section">
-                <h2>Perfil personal</h2>
-                <p>Me considero una persona responsable, creativa, con facilidad de adaptación, capacidad de trabajar en equipo y con ganas de seguir aprendiendo</p>
-            </div>
-            <div class="section">
-                <h2>Formación</h2>
-                <p><strong>(2015-2020) ESO</strong><br>
-                I.E.S Fernando De Mena, Socuéllamos</p>
-                <p><strong>(2021-2022) GRADO MEDIO SISTEMAS MICROINFORMATICOS Y REDES</strong><br>
-                I.E.S. Juan Bosco, Alcázar de San Juan</p>
-                <p><strong>(2022-Actualmente) GRADO SUPERIOR DESARROLLO DE APLICACIONES WEB</strong><br>
-                I.E.S. Juan Bosco, Alcázar de San Juan</p>
-            </div>
-            <div class="section">
-                <h2>Experiencia Laboral</h2>
-                <p><strong>Informático, Ayuntamiento de Socuéllamos</strong><br>
-                04/07/2022 - 30/06/2023</p>
-                <ul>
-                    <li>Reparación y Mantenimiento de equipos informáticos</li>
-                    <li>Mantenimiento de redes</li>
-                    <li>Instalación de Sistemas Operativos</li>
-                    <li>Instalación de Programas</li>
-                    <li>Mantenimiento del Servidor y Dominio</li>
-                    <li>Telefonía</li>
-                    <li>Mantenimiento de cuentas de correo electrónico</li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <footer>
-        <p>&copy; 2024 Lazos De Esperanza</p>
-    </footer>
+        <footer>
+            <p>&copy; 2024 Lazos De Esperanza</p>
+        </footer>
 </body>
 
 </html>
